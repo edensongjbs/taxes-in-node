bf803505918b02eb979c8c6a2c04486722a14ca5
edensongjbs <edensongjbs@gmail.com>
2020-09-30 17:46:14 -0400

    rev expense type schema

--- models/ExpenseType.model.js
+++ models/ExpenseType.model.js
@@ -2,39 +2,19 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const date = require('date-and-time')
 
-const ExpenseSchema = new Schema({
-    status:String,
-    date: Date,
-    description: String,
-    debit: Schema.Types.Decimal128,
-    credit: Schema.Types.Decimal128,
-    coeff:{type: Schema.Types.Decimal128, default:1.0},
-    business: Boolean,
-    category: Schema.Types.ObjectId,
-    paymentSource: Schema.Types.ObjectId,
-    notes:String
+const ExpenseTypeSchema = new Schema({
+    categoryName:String,
+    categoryChar:String,
+    description: {type:String, default:"A default expense description"}
 })
 
-ExpenseSchema.methods.setCategory = function(json) {
 
-}
-
-ExpenseSchema.methods.setPaymentSource = function(json) {
-
-}
 
 ExpenseSchema.methods.newFromJson = function(json) {
-        this.status = json.Status
-        // console.log('parsing a date', this )
-        this.date = new Date(date.parse(json.Date, 'MM/DD/YYYY'))
-        this.debit = parseFloat(json.Debit) || parseFloat(json.Credit)
-        this.business = json.business
-        this.category = this.setCategory(json.category)
-        this.paymentSource = this.setPaymentSource(json.paymentSource)
-        this.notes = json.notes
-        this.coeff = json.coeff || 1.0
+        this.categoryName = json.categoryName
+        this.categoryChar = json.categoryChar
         this.save()
         return this
 }
 
-module.exports = mongoose.model('Expense', ExpenseSchema)
+module.exports = mongoose.model('ExpenseType', ExpenseTypeSchema)
